e0bc2fe2f480916f650855566f61d96e9da14faf
Ioannis Charalampidis <icharalampidis@mesosphere.com>
2019-09-04 18:43:26 +0300

    Fixing package version on main

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"github.com/hashicorp/terraform/plugin"
-	"github.com/rolyv/terraform-provider-marathon/marathon"
+	"github.com/mesosphere/terraform-provider-marathon/marathon"
 )
 
 func main() {
